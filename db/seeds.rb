ba0689d54fad4f9f3ec05e4107974e5c6411b430
Lucas Otsuka <otsuka.lucas@gmail.com>
2018-11-01 17:38:56 -0300

    doses and cocktails ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+
+Cocktail.create(name: 'Martini')
+Cocktail.create(name: 'Cuba Libre')
+Cocktail.create(name: 'Tequila Shot')
